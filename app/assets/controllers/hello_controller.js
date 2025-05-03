4cf242b892e91cd7a847b9a138f13f24012604cf
Martyna Zawada <martyna.zawada@host.com>
2025-05-03 11:39:25 +0200

    zmiany w konfiguracji -  narzedzia do analizy kodu

--- app/assets/controllers/hello_controller.js
+++ app/assets/controllers/hello_controller.js
@@ -3,9 +3,9 @@ import { Controller } from '@hotwired/stimulus';
 /*
  * This is an example Stimulus controller!
  *
- * Any element with a data-controller="hello" attribute will cause
- * this controller to be executed. The name "hello" comes from the filename:
- * hello_controller.js -> "hello"
+ * Any element with a data-controller="HelloController.php" attribute will cause
+ * this controller to be executed. The name "HelloController.php" comes from the filename:
+ * hello_controller.js -> "HelloController.php"
  *
  * Delete this file or adapt it for your use!
  */
